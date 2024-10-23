e8f4d270221aa5339ffd6fb986e0471141f0ab5a
admin <admin@DESKTOP-FA5KDQG>
2024-10-23 18:31:36 +0300

    Add project files.

---
+++ AdditionalTask2.cs
@@ -0,0 +1,121 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_HW_25_10
+{
+    public class AdditionalTask2
+    {
+        private static string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=EFCoreDB;Trusted_Connection=True;";
+        static void Main(string[] args)
+        {
+            /*IEnumerable<object> books = GetBooksByAuthor(new Author() { Id = 1 });
+            DeleteCheapiestBookInCategory(new Category() { Id = 1 });
+            //IncreaseBooksPrice();
+            var books = GetBooksByPriceRange(50, 100);
+            var bookCount = GetBookCountPerAuthor();*/
+        }
+
+        static IEnumerable<object> GetBooksByAuthor(Author author)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = """
+                    SELECT Title, a.FullName, c.Name, Price, PublishedOn
+                    FROM Books
+                    JOIN Authors as a ON a.Id = AuthorId
+                    JOIN Categories as c ON c.Id = CategoryId
+                    WHERE a.Id = @Id
+                    """;
+                return db.Query(sqlQuery, author);
+            }
+        }
+
+        static void DeleteCheapiestBookInCategory(Category category)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = """
+                    DELETE FROM Books
+                    WHERE Id = (
+                        SELECT TOP 1 Id
+                        FROM Books
+                        WHERE CategoryId = @Id
+                        ORDER BY Price
+                    )
+                    """;
+                db.Execute(sqlQuery, category);
+            }
+        }
+
+        static void IncreaseBooksPrice()
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = """
+                    UPDATE Books SET Price = Price * 1.05
+                    """;
+                db.Execute(sqlQuery);
+            }
+        }
+
+        static IEnumerable<Book> GetBooksByPriceRange(int start, int end)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = """
+                    SELECT * FROM Books
+                    WHERE Price BETWEEN @start AND @end
+                    """;
+                return db.Query<Book>(sqlQuery, new { start, end });
+            }
+        }
+
+        static IEnumerable<dynamic> GetBookCountPerAuthor()
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                string sqlQuery = """
+                    SELECT a.Id AS AuthorId, a.FullName AS AuthorName, COUNT(b.Id) AS BooksCount
+                    FROM Authors a
+                    JOIN Books b ON a.Id = b.AuthorId
+                    GROUP BY a.Id, a.FullName
+                    """;
+                return db.Query(sqlQuery);
+            }
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public List<Book> Books { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=EFCoreDB;Trusted_Connection=True;";
+    static void Main(string[] args)
+    {
+        AddTask(new TaskModel { Title = "New Task 1", Description = "New Description 1", DueDate = DateTime.Now, IsCompleted = false });
+        AddTask(new TaskModel { Title = "New Task 2", Description = "New Description 2", DueDate = DateTime.Now, IsCompleted = true });
+        AddTask(new TaskModel { Title = "New Task 3", Description = "New Description 3", DueDate = DateTime.Now, IsCompleted = false });
+        DeleteTask(3);
+        var tasks = GetTasks();
+        UpdateTask(new TaskModel { Id = 2, Title = "Updated", Description = "Updated description", DueDate = DateTime.Now.AddDays(1), IsCompleted = true });
+    }
+
+    static void AddTask(TaskModel task)
+    {
+        using (IDbConnection db = new SqlConnection(connectionString))
+        {
+            var sqlQuery = """
+            INSERT INTO Tasks (Title, Description, DueDate, IsCompleted)
+                   VALUES (@Title, @Description, @DueDate, @IsCompleted);
+            SELECT SCOPE_IDENTITY()
+            """;
+            db.Execute(sqlQuery, task);
+        }
+    }
+
+    static void DeleteTask(int id)
+    {
+        using (IDbConnection db = new SqlConnection(connectionString))
+        {
+            var sqlQuery = """
+            DELETE FROM Tasks WHERE Id = @id;
+            """;
+            db.Execute(sqlQuery, new { id });
+        }
+    }
+    static IEnumerable<TaskModel> GetTasks()
+    {
+        using (IDbConnection db = new SqlConnection(connectionString))
+        {
+            var sqlQuery = """
+            SELECT * FROM Tasks;
+            """;
+            return db.Query<TaskModel>(sqlQuery);
+        }
+    }
+
+    static void UpdateTask(TaskModel task)
+    {
+        using (IDbConnection db = new SqlConnection(connectionString))
+        {
+            var sqlQuery = """
+            UPDATE Tasks
+            SET Title = @Title, Description = @Description, DueDate = @DueDate, IsCompleted = @IsCompleted
+            WHERE Id = @Id
+            """;
+            db.Execute(sqlQuery, task);
+        }
+    }
+}
+
+public class TaskModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsCompleted { get; set; }
+}
